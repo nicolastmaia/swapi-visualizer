b4c022703f8e8f76076e56a699db6b3d6eda56c8
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-08 15:42:05 -0300

    header and footer removed from this page and moved to parent

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import FilmsCatalog from '../Components/FilmsCatalog';
-import Header from '../Components/Header';
-import Footer from '../Components/Footer';
 
 const Home = () => {
     return (
         <div>
-            <Header/>
             <FilmsCatalog/>
-            <Footer/>
         </div>
     )
 };
