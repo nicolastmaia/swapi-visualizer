a5b10bc355d76d6ac94e04760701e1fcf9edba1b
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-09 11:20:39 -0300

    loader component added

---
+++ src/Components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+	return (
+		<div className='flex justify-center'>
+			<div className='loader loader-bar-ping-pong is-active'></div>
+		</div>
+	);
+};
+
+export default Loader;
