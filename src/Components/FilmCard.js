2360157ab7a611703616772445c41e0ffc84c3a5
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-09 11:25:10 -0300

    wip: FilmCard styled; with test image

--- src/Components/FilmCard.js
+++ src/Components/FilmCard.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Banner from '../assets/img/teste.jpg'
 
-const FilmCard = ({film}) => {
-    return(
-        <div key = {film.episode_id}>
-            <Link to = {`/films/${film.episode_id}`}>
-            <h1>{film.title}</h1>
-        </Link>
-            <h4>{film.episode_id}</h4>
-            <h4>{film.director}</h4>
-        </div>
-    )
+const FilmCard = ({ film }) => {
+	return (
+		<div className='bg-black w-60 p-4 flex flex-col justify-center items-center' key={film.episode_id}>
+			<div>
+				<img className='w-full h-72 bg-cover' src={Banner} alt={film.title} />
+			</div>
+			<div className='text-center'>
+				<Link to={`/films/${film.episode_id}`}>
+					<div style={{ color: '#c81a2b' }}>Star Wars: {film.title}</div>
+				</Link>
+				<div style={{ color: '#fff' }}>Episode: {film.episode_id}</div>
+				<div style={{ color: '#fff' }}>Director: {film.director}</div>
+				<Link to={`/films/${film.episode_id}`}>
+					<div className='bg-blue-500 mt-2 text-white p-2 flex justify-center w-full'>Show More</div>{' '}
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default FilmCard;
