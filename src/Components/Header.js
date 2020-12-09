9a925f2ad44df54861438840298c474709779f21
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-09 11:22:59 -0300

    header styled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Header = () => {
-	return <div></div>;
+	return (
+		<div className='border-b h-28 flex justify-center items-center bg-black'>
+			<span className='font-bold text-6xl' style={{ color: '#ffe81f '}}>
+				Cool Name Here
+			</span>
+		</div>
+	);
 };
 
 export default Header;
