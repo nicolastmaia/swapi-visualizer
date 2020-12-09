27faa82e056733d93db30e7b97c49f4dfdc3fcee
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-09 11:20:11 -0300

    tailwind reinstalled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./src/**/*.js', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: { backgroundColor: { 'star-wars-purple': '#20164d' } },
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
