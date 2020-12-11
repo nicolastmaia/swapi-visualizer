042226c83d71a358c8d9c4803bc3c45f00d43e99
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-12-11 10:11:52 -0300

    Substituted custom components with antd components

--- src/Components/FilmCard.js
+++ src/Components/FilmCard.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Banner from '../assets/img/teste.jpg'
+import { Button, Card } from 'antd';
+import Banner from '../assets/img/teste.jpg';
+import Meta from 'antd/lib/card/Meta';
 
 const FilmCard = ({ film }) => {
 	return (
-		<div className='bg-black w-60 p-4 flex flex-col justify-center items-center' key={film.episode_id}>
-			<div>
-				<img className='w-full h-72 bg-cover' src={Banner} alt={film.title} />
-			</div>
-			<div className='text-center'>
-				<Link to={`/films/${film.episode_id}`}>
-					<div style={{ color: '#c81a2b' }}>Star Wars: {film.title}</div>
-				</Link>
-				<div style={{ color: '#fff' }}>Episode: {film.episode_id}</div>
-				<div style={{ color: '#fff' }}>Director: {film.director}</div>
-				<Link to={`/films/${film.episode_id}`}>
-					<div className='bg-blue-500 mt-2 text-white p-2 flex justify-center w-full'>Show More</div>{' '}
-				</Link>
-			</div>
-		</div>
+		<Card hoverable style={{ borderColor: '#c3c3c3', backgroundColor: '#c3c3c3', width: 180 }} cover={<img alt='example' src={Banner} />}>
+			<Meta title={'Star Wars: ' + film.title} description={'Episode: ' + film.episode_id} />
+			<Link to={`/films/${film.episode_id}`}>
+				<Button style={{ marginTop: '1rem' }} type='primary'>
+					Show More
+				</Button>
+			</Link>
+		</Card>
 	);
 };
 
